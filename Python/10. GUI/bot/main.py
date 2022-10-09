149909c59e4647ce0f8dc8eca8d613580658242d
Titodev <daniel.guardado@outlook.es>
2022-10-09 21:51:01 +0200

    Update 09/10/22

---
+++ Python/10. GUI/bot/main.py
@@ -0,0 +1,4 @@
+from instabot import Bot
+
+mi_bot = Bot()
+mi_bot.login(username='')
--- Python/10. GUI/ejercicio1/main.py
+++ Python/10. GUI/ejercicio1/main.py
@@ -1,24 +1,27 @@
 import tkinter
-from tkinter import ttk
 
 window = tkinter.Tk()
 selected = tkinter.StringVar()
 
 lista = ['Windows', 'macOS', 'Linux', 'MS DOS', 'AmigaOS', 'BeOS', 'DS/2']
-lista_items = tkinter.StringVar(value=0)
+lista_items = tkinter.StringVar(window, '1')
+
+
+def seleccionar():
+    print(lista[int(lista_items.get()) - 1])
+
+
+def reset():
+    lista_items.set(None)
+
+
 contador = 0
+
 for nombre in lista:
-    r = tkinter.Radiobutton(window, text=nombre, value=contador + 1, variable=lista_items)
+    r = tkinter.Radiobutton(window, text=nombre, value=contador + 1, variable=lista_items, command=seleccionar)
     r.grid(column=0, row=contador, sticky=tkinter.W)
     contador += 1
 
-
-def reiniciar():
-    global lista_items = tkinter.StringVar(value=0)
-    contador = 0
-
-
-button = tkinter.Button(window, text="Reiniciar")
-button.bind('<Button-1>', reiniciar())
+button = tkinter.Button(window, text="Reiniciar", command=reset)
 button.grid(column=1)
 window.mainloop()
