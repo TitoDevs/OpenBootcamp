ccb20377f6d49ea67a48e484da281736cb8653ad
Titodev <daniel.guardado@outlook.es>
2022-11-28 15:24:43 +0100

    Create primitivos.js

---
+++ JavaScript Básico/2. Sintaxis, variables y palabras reservadas de JS/primitivos.js
@@ -0,0 +1,26 @@
+// Tipos primitivod
+
+// number
+4;
+0;
+
+// string
+"Hola mundo";
+'Hola mundo';
+`Hola mundo`;
+
+// booleanos
+true;
+false;
+
+// nulo y undefined
+null;
+undefined;
+
+// null, undefined, false, 0 ===> Todas son false
+console.log(null === undefined)
+if(true){
+    console.log("cumple")
+} else {
+    console.log("no cumple")
+}
