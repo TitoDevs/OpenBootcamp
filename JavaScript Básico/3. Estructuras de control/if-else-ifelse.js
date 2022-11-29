c0703bc826ec789b603dc1e221551e56db6eca0e
Titodev <daniel.guardado@outlook.es>
2022-11-29 22:46:07 +0100

    Update 29/11/22

---
+++ JavaScript Básico/3. Estructuras de control/if-else-ifelse.js
@@ -0,0 +1,16 @@
+// if else + if else
+let nota = 5;
+
+if (nota === 5) {
+    console.log("Enhorabuena, has obtenido un sobresaliente");
+} else if (nota === 4) {
+    console.log("Buen trabajo, pero podrías haberlo hecho mejor");
+} else if (nota === 3) {
+    console.log("Has obtenido un suficiente");
+} else if (nota === 2) {
+    console.log("No has aprobado por poco");
+} else if (nota === 1) {
+    console.log("no has estudiando nada, trabaja un poquito más para la próxima");
+} else {
+    console.log("Error, introduce una nota entre el 1 y el 5");
+}
---
+++ JavaScript Básico/3. Estructuras de control/if-else.js
@@ -0,0 +1,13 @@
+// Bifurcaciones if ... else
+let saldo = 50;
+let efectivo = 20;
+
+if (saldo > efectivo) {
+    console.log("Puedes sacar dinero");
+}
+
+if (efectivo < saldo) {
+    console.log("Puedes sacar dinero");
+} else {
+    console.log("Saldo insuficiente");
+}
