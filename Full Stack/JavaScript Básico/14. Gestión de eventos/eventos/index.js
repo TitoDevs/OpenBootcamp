da7ab0c2db9353daaa4c9d3d9f6b4fd1e6b5a119
Titodev <daniel.guardado@outlook.es>
2022-12-26 23:35:16 +0100

    Upload 26/12/22

---
+++ Full Stack/JavaScript Básico/14. Gestión de eventos/eventos/index.js
@@ -0,0 +1,18 @@
+const hTexto = document.getElementById("h-texto")
+console.log(hTexto);
+
+hTexto.addEventListener("cambioTexto", evento => {
+    console.log(evento.detail)
+    hTexto.innerText = evento.detail.texto
+    hTexto.style.color = evento.detail.color
+})
+
+function cambiarTexto(nuevoTexto, color) {
+    const evento = new CustomEvent("cambioTexto", {
+        detail: {
+            texto: nuevoTexto,
+            color: color
+        }
+    })
+    hTexto.dispatchEvent(evento)
+}
---
+++ Full Stack/JavaScript Básico/14. Gestión de eventos/formulario/index.js
@@ -0,0 +1,7 @@
+const f = document.getElementById("formulario")
+console.log(f)
+
+f.addEventListener("submit", evento => {
+    console.log(evento)
+    evento.preventDefault()
+})
