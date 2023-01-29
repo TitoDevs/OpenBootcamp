edcaacdbb87fdb45ee8a0f11f40011091452bb5a
Titodev <daniel.guardado@outlook.es>
2023-01-30 00:19:34 +0100

    Upload 30/01/23

---
+++ Full Stack/React/2. Componentes y jerarquía/hola-mundo/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './components/pure/greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* Componente propio greeting.jsx */}
+        <Greeting></Greeting>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Full Stack/React/2. Componentes y jerarquía/hola-mundo/src/components/pure/greeting.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Greeting extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Hola!!
+                </h1>
+            </div>
+        );
+    }
+}
+
+
+Greeting.propTypes = {
+
+};
+
+
+export default Greeting;
+
